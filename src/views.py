9488e63ec8c8ac531b3aaab9879cf9e800b14862
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-23 00:30:27 +0330

    view and templates

--- src/views.py
+++ src/views.py
@@ -1,5 +1,5 @@
 import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 
 # Create your views here.
@@ -8,3 +8,12 @@ from .models import Todo
 def Home(request):
     context = Todo.objects.all()
     return render(request, 'home.html', {"Todos": context})
+
+def Delete(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    return redirect('home')
+
+def Done(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    return redirect('home')
+    
