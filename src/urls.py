0e0b90b45e4b92a33c2420f5d5972e143591f011
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-23 00:30:12 +0330

    done and delete url

--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.Home, name='home')
-    
+    path('', views.Home, name='home'),
+    path('delete/<int:todo_id>/', views.Delete, name='delete'),
+    path('done/<int:todo_id>/', views.Done, name='done'),
 ]
