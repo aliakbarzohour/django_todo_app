6354978ac526fc2e310f6e180892df8977417b78
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-24 00:13:57 +0330

    edit todo edition

--- src/forms.py
+++ src/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 import datetime
+from .models import Todo
 
 class CreateTodo(forms.Form):
     title = forms.CharField(max_length=200, label='عنوان')
     sub_title = forms.CharField(max_length=100, label='درباره')
     body = forms.CharField(label='توضیحات')
     date = forms.DateTimeField(label='زمان ساختن', required=False)
+
+class EditTodo(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ('title','body', 'sub_title')
--- src/urls.py
+++ src/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('', views.Home, name='home'),
     path('delete/<int:todo_id>/', views.Delete, name='delete'),
     path('done/<int:todo_id>/', views.Done, name='done'),
-    path('create/', views.Create, name='create')
+    path('create/', views.Create, name='create'),
+    path('edit/<int:todo_id>/', views.Edit, name='edit'),
 ]
