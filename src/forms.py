da44b38b7e0c5553a9512cc053c5f12899963566
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-23 14:04:08 +0330

    create forms

---
+++ src/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+import datetime
+
+class CreateTodo(forms.Form):
+    title = forms.CharField(max_length=200, label='عنوان')
+    sub_title = forms.CharField(max_length=100, label='درباره')
+    body = forms.CharField(label='توضیحات')
+    date = forms.DateTimeField(label='زمان ساختن',required=False)
+    if 'date' == '':
+        date = datetime.now()
