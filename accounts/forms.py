b6f16229e7c495f75d2b40d62aed2feabfb75f33
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-27 00:09:09 +0330

    user login class

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,3 +4,7 @@ class UserRegistrationForm(forms.Form):
     username = forms.CharField()
     email = forms.EmailField()
     password = forms.CharField(max_length=100)
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
