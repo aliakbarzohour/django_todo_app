ed0b1e83ee086128ed80cb5f58f1e2f46c541ac8
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-26 22:12:05 +0330

    update views

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import UserRegistrationForm
 
 # Create your views here.
 
 
 def user_register(request):
-    return HttpResponse('User Register Page ....')
+    if request.method == 'POST':
+        pass
+    else:
+        form = UserRegistrationForm()
+    return render(request , 'register.html', {'form':form})
