e8efc9c566ff6eb80bc872181d337a05e8b6d547
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-26 23:13:12 +0330

    simple registeration user

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .forms import UserRegistrationForm
 
@@ -8,7 +9,14 @@ from .forms import UserRegistrationForm
 
 def user_register(request):
     if request.method == 'POST':
-        pass
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+                    cd = form.cleaned_data
+                    User.objects.create_user(cd['username'], cd['email'], cd['password'])
+                    messages.success(request, 'تبریک . شما با موفقیت ثبت نام شدید', 'success')
+                    return redirect('home')
+
+
     else:
         form = UserRegistrationForm()
     return render(request , 'register.html', {'form':form})
