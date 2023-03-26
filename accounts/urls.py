027074564ca7220074a93db0f72c28a58d44df79
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-26 23:03:17 +0330

    accounts url

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.user_register, name='user_register'),
+    path('register/', views.user_register, name='register'),
 ]
