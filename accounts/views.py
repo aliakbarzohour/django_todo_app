70ef180e140939fcf35b13b269c94beb5de81886
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-03-27 00:09:27 +0330

    user login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 
 # Create your views here.
 
@@ -20,3 +21,18 @@ def user_register(request):
     else:
         form = UserRegistrationForm()
     return render(request , 'register.html', {'form':form})
+
+def user_login(request):
+      if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data      
+            
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'تبریک . شما با موفقیت وارد شدید', 'success')
+                return redirect('home')
+      else:
+            form = UserLoginForm
+            return render(request, 'login.html', {'form':form}) 
